a614d73e3f86514f6a221706f4ebe2b26e86e176
Samprity Ghosh <samprityghosh234@gmail.com>
2024-08-06 15:06:26 +0530

    script.js
    gsap.from("h3",{
        Delay:0.5,
        opacity:1,
        duration:2,
        x:-60
    })
    
    gsap.from("h1",{
        Delay:0.5,
        opacity:1,
        duration:2,
        x:60
    })
    
    gsap.from("h4",{
        Delay:0.5,
        opacity:1,
        duration:2,
        x:-60
    })
    
    gsap.from("p",{
        Delay:0.5,
        opacity:1,
        duration:2,
        x:60
    })
    
    gsap.from("img",{
        Delay:0.5,
        opacity:1,
        duration:1,
        x:-60
    })
    
    gsap.from("a",{
        Delay:0.5,
        opacity:1,
        duration:1,
        y:-30
    })
    
    gsap.from("navbar a",{
        Delay:0.5,
        opacity:1,
        duration:1,
        y:-30
    })
    
    gsap.from("h5",{
        Delay:0.5,
        opacity:1,
        duration:1,
        x:60
    })
    
    gsap.to(".header", {
        duration: 1,
        opacity: 1,
        y: 0,
        ease: "power4.out"
    });
    
    gsap.from("#home",{
        Delay:0.5,
        opacity:1,
        duration:1,
        y:5
    })

---
+++ script.js
@@ -0,0 +1,69 @@
+gsap.from("h3",{
+    Delay:0.5,
+    opacity:1,
+    duration:2,
+    x:-60
+})
+
+gsap.from("h1",{
+    Delay:0.5,
+    opacity:1,
+    duration:2,
+    x:60
+})
+
+gsap.from("h4",{
+    Delay:0.5,
+    opacity:1,
+    duration:2,
+    x:-60
+})
+
+gsap.from("p",{
+    Delay:0.5,
+    opacity:1,
+    duration:2,
+    x:60
+})
+
+gsap.from("img",{
+    Delay:0.5,
+    opacity:1,
+    duration:1,
+    x:-60
+})
+
+gsap.from("a",{
+    Delay:0.5,
+    opacity:1,
+    duration:1,
+    y:-30
+})
+
+gsap.from("navbar a",{
+    Delay:0.5,
+    opacity:1,
+    duration:1,
+    y:-30
+})
+
+gsap.from("h5",{
+    Delay:0.5,
+    opacity:1,
+    duration:1,
+    x:60
+})
+
+gsap.to(".header", {
+    duration: 1,
+    opacity: 1,
+    y: 0,
+    ease: "power4.out"
+});
+
+gsap.from("#home",{
+    Delay:0.5,
+    opacity:1,
+    duration:1,
+    y:5
+})
